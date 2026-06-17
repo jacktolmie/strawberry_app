4cda02a28e7f7ff260dca3c3762759fc8e504733
Jack Tolmie <63127876+jacktolmie@users.noreply.github.com>
2026-06-17 16:02:38 -0700

    Feat: Added long press on stop for stop after current, and seek forward/backward every second on long press buttons.

--- app/src/main/java/com/example/strawberry_app/screens/playerScreen/ButtonCreators.kt
+++ app/src/main/java/com/example/strawberry_app/screens/playerScreen/ButtonCreators.kt
@@ -1,22 +1,22 @@
 package com.example.strawberry_app.screens.playerScreen
 
-import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.gestures.waitForUpOrCancellation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.PointerEventType
-import androidx.compose.ui.input.pointer.PointerInputChange
-import androidx.compose.ui.input.pointer.PointerInputEventHandler
-import androidx.compose.ui.input.pointer.PointerInputScope
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlin.time.Duration.Companion.milliseconds
@@ -37,26 +37,44 @@ fun CreateButton(image: ImageVector, description: String, control: () -> Unit) {
 }
 
 @Composable
-fun CreateLongPressButton(image: ImageVector, description: String, control: () -> Unit){
+fun CreateLongPressButton(image: ImageVector, description: String, shortPress: () -> Unit, longPress: () -> Unit) {
+    Box(modifier = Modifier
+        .combinedClickable(
+            enabled = true,
+            onClick = { shortPress() },
+            onLongClick = { longPress()}
+        )
+    ){
+        Icon(imageVector = image, contentDescription = description)
+    }
+}
+
+@Composable
+fun CreateRepeatButton(image: ImageVector, description: String, control: () -> Unit){
     Box( modifier = Modifier
-        .pointerInput(Unit) {
-            awaitPointerEventScope {
-                while (true) {
-                    val down = awaitPointerEvent()
-                    if (down.type == PointerEventType.Press) {
-                        val job = this.launch {
-                            while (true) {
-                                control()
-                                delay(1000)
-                            }
-                        }
-                        awaitPointerEvent()
-                        job.cancel()
-                    }
+        .clickable { control() }
+        .onTouchHeld { control() }
+    ) {
+        Icon(imageVector = image, contentDescription = description)
+    }
+}
+
+fun Modifier.onTouchHeld(
+    pollDelay: Long = 1000L,
+    onTouchHeld: () -> Unit
+) = composed {
+    val scope = rememberCoroutineScope()
+    pointerInput(onTouchHeld) {
+        awaitEachGesture {
+            val initialDown = awaitFirstDown(requireUnconsumed = false)
+            val job = scope.launch {
+                while (initialDown.pressed) {
+                    onTouchHeld()
+                    delay(pollDelay.milliseconds)
                 }
             }
-
-    ) {
-
+            waitForUpOrCancellation()
+            job.cancel()
+        }
     }
 }
--- app/src/main/java/com/example/strawberry_app/screens/playerScreen/PlayerScreenMedium.kt
+++ app/src/main/java/com/example/strawberry_app/screens/playerScreen/PlayerScreenMedium.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -17,15 +16,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.progressSemantics
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalSlider
@@ -39,8 +34,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -192,7 +185,7 @@ fun PlayerScreen(
         )
         {
             CreateButton(skip_previous, "Previous", callbacks.sendPrevious)
-            CreateButton(fast_rewind, "Fast Rewind", callbacks.sendSeekBackward)
+            CreateRepeatButton(fast_rewind, "Fast Rewind", callbacks.sendSeekBackward)
 
             // Show proper play or pause button, and the proper callback
             CreateButton(
@@ -204,8 +197,9 @@ fun PlayerScreen(
                 "Play / Pause",
                 if(playerValues.playState == PlayState.PLAYING) callbacks.sendPause else callbacks.sendPlay
             )
-            CreateButton(stop, "Stop", callbacks.sendStop)
-            CreateButton(fast_forward, "Fast Forward", callbacks.sendSeekForward)
+            // Send both stop and stop after current for the stop button.
+            CreateLongPressButton(stop, "Stop", callbacks.sendStop, callbacks.sendStopAfterCurrent)
+            CreateRepeatButton(fast_forward, "Fast Forward", callbacks.sendSeekForward)
             CreateButton(skip_next, "Next", callbacks.sendNext)
             }
     }
