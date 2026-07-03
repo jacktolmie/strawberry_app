63009d6f88bcb0e20f16c0229131688a7010ede5
Jack Tolmie <63127876+jacktolmie@users.noreply.github.com>
2026-07-03 14:34:03 -0700

    Chore: Created SongItem.kt for playlist song information

--- app/src/main/java/com/example/strawberry_app/screens/playlistScreen/composables/SongItem.kt
+++ app/src/main/java/com/example/strawberry_app/screens/playlistScreen/composables/SongItem.kt
@@ -7,11 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.strawberry_app.data.dao.SongWithPosition
@@ -26,35 +29,53 @@ fun SongItem(
         .background(Color.White)
         .fillMaxWidth(),
         horizontalAlignment = Alignment.Start,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         // Row for song title and length
         Row(modifier = Modifier
             .padding(start = 5.dp)
 
         ) {
-            Text(modifier = Modifier,
-                text = playlistState.currentSongData?.title ?: ""
+            SongText(
+                text = playlistState.currentSongData?.title ?: "",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1F)
             )
-            Text(modifier = Modifier,
-                text = formatTime(playlistState.currentSongData?.length ?: 1000)
+            SongText(
+                text = formatTime(playlistState.currentSongData?.length ?: 1000),
+                style = MaterialTheme.typography.bodyMedium
             )
         }
 
         // Row for artist and album name
         Row(modifier = Modifier
             .padding(start = 5.dp)
-
         ){
-            Text(
-                text = playlistState.currentSongData?.artist ?: "Artist name"
-            )
-            Text(
-                text = playlistState.currentSongData?.album ?: "Album name"
+            SongText(
+                text = playlistState.currentSongData?.artist ?: "Artist name",
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.weight(1F) )
+            SongText(
+                text = playlistState.currentSongData?.album ?: "Album name",
+                style = MaterialTheme.typography.bodyMedium
             )
         }
-
     }
+}
+
+@Composable
+fun SongText(
+    text: String,
+    style: TextStyle,
+    modifier: Modifier = Modifier
+){
+    Text(
+        modifier = modifier,
+        text = text,
+        overflow = TextOverflow.Ellipsis,
+        style = style,
+        maxLines = 1
+    )
 }
 
 @Preview
